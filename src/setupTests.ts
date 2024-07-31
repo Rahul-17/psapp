edd35bb6d64a1a57a786c77968e594b567b23be0
cph3cob <153836731+cph3cob@users.noreply.github.com>
2024-07-31 09:44:05 +0530

    Update setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,12 +3,37 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-
-// Mock matchmedia
-window.matchMedia = window.matchMedia || function() {
+ 
+// Mock matchMedia
+window.matchMedia = window.matchMedia || function(query: string): MediaQueryList {
   return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {}
-  };
+    matches: false,
+    media: query,
+    onchange: null,
+    // Implementation for adding listeners
+    addListener(listener: (this: MediaQueryList, ev: MediaQueryListEvent) => any): void {      
+    },
+    // Implementation for removing listeners
+    removeListener(listener: (this: MediaQueryList, ev: MediaQueryListEvent) => any): void {      
+    },
+    // Implementation for adding event listeners
+    addEventListener: function<K extends keyof MediaQueryListEventMap>(
+      type: K,
+      listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => any,
+      options?: boolean | AddEventListenerOptions
+    ): void {
+      
+    },
+    // Implementation for removing event listeners
+    removeEventListener: function<K extends keyof MediaQueryListEventMap>(
+      type: K,
+      listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => any,
+      options?: boolean | EventListenerOptions
+    ): void {
+      
+    },
+    dispatchEvent: function(event: Event): boolean {
+      return false;
+    }
+  } as MediaQueryList;
 };
